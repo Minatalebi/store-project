1d5bea6ad74b489b869e6e36d6720f53f13c1202
Mina Talebi <mina.talebi726@gmail.com>
2025-11-21 21:20:48 +0330

    add component card product

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { useProducts } from "../context/ProductsProvider";
-
+import styles from "./ProductsPage.module.css";
+import Card from "../components/Card";
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading ...</p>}
+        {products.map((p) => (
+          <Card key={p.id} data={p} />
+        ))}
+      </div>
+      <div className={styles.sidebar}>Sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
