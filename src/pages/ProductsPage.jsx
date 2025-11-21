b67c26f67fc68525de2cb17d61e3f922a1b48ead
Mina Talebi <mina.talebi726@gmail.com>
2025-11-21 22:19:31 +0330

    add search item  and query state

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 import { useProducts } from "../context/ProductsProvider";
 import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 function ProductsPage() {
-  const products = useProducts();
+  const products = useProducts(); //koledata bara dastersi karbar
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }));
+  };
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((query) => ({ ...query, category }));
+  };
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <p>Loading ...</p>}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />{" "}
+        </button>
       </div>
-      <div className={styles.sidebar}>Sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div className={styles.sidebar}>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+            <ul onClick={categoryHandler}>
+              <li>All</li>
+              <li>Electronics</li>
+              <li>Jewelery</li>
+              <li>Men's Clothing</li>
+              <li>Women's Clothing</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
