7f16a052d88ac61ce0295183c4143b43ea62a0df
Mina Talebi <mina.talebi726@gmail.com>
2025-11-21 20:05:36 +0330

    add axios & context to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
+import ProductsProvider from "./context/ProductsProvider";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/products" replace />} />
-      //اتوماتیک ریدایرکت به صفحه محصول
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<DetailsPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        //اتوماتیک ریدایرکت به صفحه محصول
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </ProductsProvider>  
   );
 }
+
 
 export default App;
